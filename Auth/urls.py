cc499094a1af69cec568a6aa8dd02157c066b187
agent87 <arnauldkayonga1@gmail.com>
2021-07-18 20:19:12 +0200

    Update

--- Auth/urls.py
+++ Auth/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-    path('login.html', views.login_page, name='login_page'),
-    path('dashboard.html', views.dashboard_page, name='dashboard_page'),
-    path('history.html', views.history_page, name='history_page'),
-    path('pricing.html', views.pricing_page, name='pricing_page'),
-    path('pricing.html', views.pricing_page, name='pricing_page'),
-    path('parked.html', views.today_page, name='today_page'),
-    path('user.html', views.user_page, name='user_page'),
+urlpatterns = [    
+    path('', views.responses.login_page, name='login_page'),
+    path('login.html', views.responses.login_page, name='login_page'),
+    path('dashboard.html', views.responses.dashboard_page, name='dashboard_page'),
+    path('history.html', views.responses.history_page, name='history_page'),
+    path('pricing.html', views.responses.pricing_page, name='pricing_page'),
+    path('parked.html', views.responses.parked_page, name='parked_page'),
+    path('user.html', views.responses.user_page, name='user_page'),
 ]
--- Auth/views.py
+++ Auth/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 import os
 import psycopg2
 
@@ -8,68 +9,78 @@ try:
 except:
     conn = psycopg2.connect(database="d7pibsdo79jogi",host="ec2-52-86-25-51.compute-1.amazonaws.com",port=5432,user="cccbiffnldwfkf",password="605444bcd83d702da6e7f56cb2fba0ebb74fb3db14dc5a0c1555bbfa75a357a1")
 
-
 # Create your views here.
-def login_page(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    
-    return render(request, 'Auth/login.html')
-
-def dashboard_page(request):
-    context = dict
-    with conn.cursor() as cursor:
-        try:
-            cursor.execute("""SELECT  "Date", "PlateNum", "EntryGateId", "CheckinTime", "CheckoutTime", "ExitGateId", "Status", "Duration", "Cash"
-                          FROM public."ParkingLog"
-                          WHERE "CustomerId" = 'EGPCI-AAA01-0001';""")
-        except:
-            f'Unable To retrive Data'
-                          
-    return render(request, 'Auth/dashboard.html')
-
-def history_page(request):
-    context = dict
-    with conn.cursor() as cursor:
-        cursor.execute("""SELECT  "Date", "PlateNum", "EntryGateId", "CheckinTime", "CheckoutTime", "ExitGateId", "Status", "Duration", "Cash"
-                          FROM public."ParkingLog"
-                          WHERE "CustomerId" = 'EGPCI-AAA01-0001';""")
-        
-        ParkingLogs = []
-        for i in cursor.fetchall():
-            ParkingLog = {"Date": i[0]}
-            ParkingLog['platenum'] = i[1]
-            ParkingLog['entrygate'] = i[2]
-            ParkingLog['checkintime'] = i[3]
-            ParkingLog['checkouttime'] = i[4]
-            ParkingLog['exitgate'] = i[5]
-            ParkingLog['status'] = i[6]
-            ParkingLog['duration'] = i[7]
-            ParkingLog['cash'] = i[8]
+class responses:
+    def login_page(request):
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            if username == 'admin':
+                if password == 'admin123':
+                    request.session['user_id'] = '20'
+                    return redirect(responses.dashboard_page)
             
-            ParkingLogs.append(ParkingLog) 
-    context = {"ParkingLogs":ParkingLogs}
-    return render(request, 'Auth/history.html',context)
+            else:
+                return render(request, 'Auth/login.html')
+        else:
+            return render(request, 'Auth/login.html')
 
-def pricing_page(request):
-    context = dict
-    with conn.cursor() as cursor:
-        cursor.execute("""SELECT "FromTime", "ToTime", "Cost"
-                            FROM public."Tarrif"
-                            where "CustomerId" = 'EGPCI-AAA01-0001';""")
-        tarrifs = []
-        for i in cursor.fetchall():            
-            tarrif = {}
-            #tarrif['Id'] = i
-            tarrif['fromtime'] = i[0]
-            tarrif['totime'] = i[1]
-            tarrif['cost'] = i[2]
-            tarrifs.append(tarrif)         
-    context = {'tarrifs' : tarrifs} 
-    return render(request, 'Auth/pricing.html', context)
 
-def today_page(request):
-    return render(request, 'Auth/parked.html')
+    def dashboard_page(request):
+        print(request.session.get('user_id'))
+        context = dict
+        with conn.cursor() as cursor:
+            try:
+                cursor.execute("""SELECT  "Date", "PlateNum", "EntryGateId", "CheckinTime", "CheckoutTime", "ExitGateId", "Status", "Duration", "Cash"
+                            FROM public."ParkingLog"
+                            WHERE "CustomerId" = 'EGPCI-AAA01-0001';""")
+            except:
+                f'Unable To retrive Data'
+                            
+        return render(request, 'Auth/dashboard.html')
 
-def user_page(request):
-    return render(request, 'Auth/user.html')
+    def history_page(request):
+        context = dict
+        with conn.cursor() as cursor:
+            cursor.execute("""SELECT  "Date", "PlateNum", "EntryGateId", "CheckinTime", "CheckoutTime", "ExitGateId", "Status", "Duration", "Cash"
+                            FROM public."ParkingLog"
+                            WHERE "CustomerId" = 'EGPCI-AAA01-0001';""")
+            
+            ParkingLogs = []
+            for i in cursor.fetchall():
+                ParkingLog = {"Date": i[0]}
+                ParkingLog['platenum'] = i[1]
+                ParkingLog['entrygate'] = i[2]
+                ParkingLog['checkintime'] = i[3]
+                ParkingLog['checkouttime'] = i[4]
+                ParkingLog['exitgate'] = i[5]
+                ParkingLog['status'] = i[6]
+                ParkingLog['duration'] = i[7]
+                ParkingLog['cash'] = i[8]
+                
+                ParkingLogs.append(ParkingLog) 
+        context = {"ParkingLogs":ParkingLogs}
+        return render(request, 'Auth/history.html',context)
+
+    def pricing_page(request):
+        context = dict
+        with conn.cursor() as cursor:
+            cursor.execute("""SELECT "FromTime", "ToTime", "Cost"
+                                FROM public."Tarrif"
+                                where "CustomerId" = 'EGPCI-AAA01-0001';""")
+            tarrifs = []
+            for i in cursor.fetchall():            
+                tarrif = {}
+                #tarrif['Id'] = i
+                tarrif['fromtime'] = i[0]
+                tarrif['totime'] = i[1]
+                tarrif['cost'] = i[2]
+                tarrifs.append(tarrif)         
+        context = {'tarrifs' : tarrifs} 
+        return render(request, 'Auth/pricing.html', context)
+
+    def parked_page(request):
+        return render(request, 'Auth/parked.html')
+
+    def user_page(request):
+        return render(request, 'Auth/user.html')

503898ebe1452413de22704e150fde0ffb9dfa4c
agent87 <arnauldkayonga1@gmail.com>
2021-07-19 20:58:20 +0200

    Up

--- Auth/models.py
+++ Auth/models.py
@@ -22,7 +22,7 @@ class dashboard_page_context:
         with conn.cursor() as cursor:
             cursor.execute("""SELECT  SUM("Cash") FROM public."ParkingLog" WHERE "CustomerId" = '{}' and "Status" = 'Exited' and date("Date") = CURRENT_DATE;""".format(CustomerId))
             revenue = {'today':cursor.fetchall()[0][0]}
-            cursor.execute("""SELECT  COUNT("Cash") FROM public."ParkingLog" WHERE "CustomerId" = '{}';""".format(CustomerId))
+            cursor.execute("""SELECT  COUNT(*) FROM public."ParkingLog";""".format(CustomerId))
             revenue['lastweek']=cursor.fetchall()[0][0]
             print(revenue)
             
